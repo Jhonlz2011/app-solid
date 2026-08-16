5c309de1f979d89ca717969cb898acc3a0fd10f7
jhonlz09 <jhonlz2011@hotmail.com>
2026-08-15 23:16:08 -0500

    entities v90

--- frontend/src/modules/categories/components/CategoryAttributesPicker.tsx
+++ frontend/src/modules/categories/components/CategoryAttributesPicker.tsx
@@ -25,7 +25,7 @@ import { AttributeSelect } from '@shared/ui/selectors/AttributeSelect';
 import Checkbox from '@shared/ui/Checkbox';
 import { Badge } from '@shared/ui/Badge';
 import { CloseIcon, GripVerticalIcon } from '@shared/ui/icons';
-import { useAttributeList } from '../data/attributes.queries';
+import { useAttributeList } from '@modules/attributes/data/attributes.queries';
 
 interface CategoryAttributesPickerProps {
     value: CategoryAttributeEntry[];
